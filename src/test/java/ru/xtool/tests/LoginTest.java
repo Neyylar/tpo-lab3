25c6d5b6d3f4753c0064b60ad49af71ed88f6adc
neyylar <natashata7sen@gmail.com>
2022-09-28 13:36:47 +0300

    Refactored code

--- src/test/java/ru/xtool/tests/LoginTest.java
+++ src/test/java/ru/xtool/tests/LoginTest.java
@@ -1,16 +1,10 @@
 package ru.xtool.tests;
-import com.codeborne.selenide.SelenideConfig;
 import com.codeborne.selenide.SelenideDriver;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
-import org.junit.jupiter.params.provider.MethodSource;
 import ru.xtool.pages.MainPage;
-
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
